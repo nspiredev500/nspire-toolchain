a6c7b75ada280fb27af3b780bc430c8870e3ff12
nspiredev500 <57017842+nspiredev500@users.noreply.github.com>
2020-07-05 21:29:37 +0200

    worked on the assembler

--- n-as/n-as.c
+++ n-as/n-as.c
@@ -10,17 +10,124 @@
 #include <strings.h>
 #include <string.h>
 
+bool section_write(struct section* s,void* data,uint32_t size,int offset);
+
+const char* error = NULL; // if a function sets this, and error occurred and is displayed
+
+int linelength(const char* line)
+{
+	int count = 0;
+	while (*line != '\0' && *line != '\n')
+	{
+		count++;
+	}
+	return count;
+}
+
+
 // one assemble function for every family of instructions
 // line is a pointer into the assembly string, inst is a pointer to where the opcode should be placed
 // the return value is the number of characters until after the next \n, or -1 if and \0 is encountered
-int assemble_mov_arm(const char* line,uint32_t *inst);
-int assemble_add_arm(const char* line,uint32_t *inst);
-int assemble_sub_arm(const char* line,uint32_t *inst);
-int assemble_ldr_arm(const char* line,uint32_t *inst);
-int assemble_str_arm(const char* line,uint32_t *inst);
-int assemble_tst_arm(const char* line,uint32_t *inst);
-int assemble_cmp_arm(const char* line,uint32_t *inst);
-int assemble_b_arm(const char* line,uint32_t *inst);
+// inst is a void pointer, because alignment can't be guaranteed by the assembly the user provides
+int assemble_mov_arm(const char* line,void* inst);
+int assemble_add_arm(const char* line,void* inst);
+int assemble_sub_arm(const char* line,void* inst);
+int assemble_ldr_arm(const char* line,void* inst);
+int assemble_str_arm(const char* line,void* inst);
+int assemble_tst_arm(const char* line,void* inst);
+int assemble_cmp_arm(const char* line,void* inst);
+int assemble_b_arm(const char* line,void* inst);
+int assemble_word(const char* line,void* inst);
+int assemble_short(const char* line,void* inst);
+int assemble_byte(const char* line,void* inst);
+
+
+// return value the same as above
+int directive_section(const char* line,bool* thumb, uint32_t* current_section)
+{
+	int len = linelength(line);
+	if (len == strlen(".arm"))
+	{
+		if (line[0] == '.' && line[1] == 'a' &&  line[2] == 'r' && line[3] == 'm')
+		{
+			/// TODO somehow take care about the alignment while not leaving holes in the binary
+			/*
+			if (((uint32_t) (sections[*current_section].nextindex) & 0b11) != 0)
+			{
+				sections[*current_section].nextindex &= ~0b11; // clear the lowest 2 bits
+				sections[*current_section].nextindex += 0b100;
+			}
+			*/
+			*thumb = false;
+			if (line[len] == '\0')
+			{
+				return -1;
+			}
+			else
+			{
+				return len+1; // len points to the newline
+			}
+		}
+	}
+	if (len == strlen(".thumb"))
+	{
+		if (line[0] == '.' && line[1] == 't' &&  line[2] == 'h' && line[3] == 'u' && line[4] == 'm' && line[5] == 'b')
+		{
+			/// TODO somehow take care about the alignment while not leaving holes in the binary
+			*thumb = true;
+			if (line[len] == '\0')
+			{
+				return -1;
+			}
+			else
+			{
+				return len+1; // len points to the newline
+			}
+		}
+	}
+	if (len > 1)
+	{
+		if (line[0] == '.')
+		{
+			for (uint32_t i = 1;i<len;i++)
+			{
+				if (line[i] == ':')
+				{
+					if (line[len] == '\0')
+					{
+						return -1;
+					}
+					else
+					{
+						return len+1; // len points to the newline
+					}
+				}
+			}
+			struct section *s = malloc(struct section);
+			if (s == NULL)
+			{
+				error = "Out of Memory!";
+				return -1;
+			}
+			s->name = line;
+			s->lenght = len;
+			s->data = NULL;
+			s->size = 0;
+			s->nextindex = 0;
+			add_section(s);
+		}
+	}
+	error = "unknown directive";
+	if (line[len] == '\0')
+	{
+		return -1;
+	}
+	else
+	{
+		return len+1; // len points to the newline
+	}
+}
+
 
 
 struct section {
@@ -29,7 +136,7 @@ struct section {
 	uint32_t offset; // offset from the start of the binary. one section is selected as the start of the binary, and all others are relative to it
 	void* data; // the contents of the section
 	uint32_t size; // size of the section at this time
-	void* nextindex; // if (data-nextindex) > size and something has to be put in the section, the section is made bigger
+	uint32_t nextindex; // if (data+nextindex) > size and something has to be put in the section, the section is made bigger
 };
 
 
@@ -49,13 +156,66 @@ struct label {
 	uint8_t section; // index into the section table
 };
 
-struct section *sections = NULL;
+
+// if offset is -1, nextindex is used
+bool section_write(struct section* s,void* data,uint32_t size,int offset)
+{
+	if (s->data == NULL)
+	{
+		s->data = malloc(50);
+		if (s->data == NULL)
+		{
+			error = "Out of Memory!";
+			return false;
+		}
+		s->size = 50;
+	}
+	if (offset == -1)
+	{
+		if (s->nextindex+size >= s->size)
+		{
+			uint32_t diff = ((s->nextindex+size)-s->size)+5;
+			void* new_s_data = malloc(s->size+diff);
+			if (new_s_data == NULL)
+			{
+				error = "Out of Memory!";
+				return false;
+			}
+			memset(new_s_data,'\0',s->size+diff);
+			memcpy(new_s_data,s->data,s->size);
+			free(s->data);
+			s->data = new_s_data;
+		}
+		memcpy(s->data,data,size);
+		s->nextindex += size;
+	}
+	else
+	{
+		if (offset+size >= s->size)
+		{
+			uint32_t diff = ((offset+size)-s->size)+5;
+			void* new_s_data = malloc(s->size+diff);
+			if (new_s_data == NULL)
+			{
+				error = "Out of Memory!";
+				return false;
+			}
+			memset(new_s_data,'\0',s->size+diff);
+			memcpy(new_s_data,s->data,s->size);
+			free(s->data);
+			s->data = new_s_data;
+		}
+		memcpy(s->data,data,size);
+	}
+}
+
+struct section **sections = NULL;
 uint32_t sections_size = 0;
 uint32_t next_section = 0;
-struct fixup *fixups = NULL;
+struct fixup **fixups = NULL;
 uint32_t fixups_size = 0;
 uint32_t next_fixup = 0;
-struct label *labels = NULL;
+struct label **labels = NULL;
 uint32_t labels_size = 0;
 uint32_t next_label = 0;
 
@@ -66,6 +226,7 @@ bool add_section(struct section *sect)
 		sections = malloc(sizeof(void*)*10);
 		if (sections == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		sections_size = 10;
@@ -76,9 +237,10 @@ bool add_section(struct section *sect)
 	}
 	if (next_section >= sections_size)
 	{
-		struct section* new_sections = malloc(sections_size*2);
+		struct section** new_sections = malloc(sections_size*2);
 		if (new_sections == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		for (uint32_t i = 0;i<sections_size*2;i++)
@@ -94,6 +256,7 @@ bool add_section(struct section *sect)
 		sections_size = sections_size*2;
 	}
 	sections[next_section] = sect;
+	return true;
 }
 
 bool add_fixup(struct fixup *fix)
@@ -103,6 +266,7 @@ bool add_fixup(struct fixup *fix)
 		fixups = malloc(sizeof(void*)*10);
 		if (fixups == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		fixups_size = 10;
@@ -113,9 +277,10 @@ bool add_fixup(struct fixup *fix)
 	}
 	if (next_fixup >= fixups_size)
 	{
-		struct fixup* new_fixups = malloc(fixups_size*2);
+		struct fixup** new_fixups = malloc(fixups_size*2);
 		if (new_fixups == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		for (uint32_t i = 0;i<fixups_size*2;i++)
@@ -131,6 +296,7 @@ bool add_fixup(struct fixup *fix)
 		fixups_size = fixups_size*2;
 	}
 	fixups[next_fixup] = fix;
+	return true;
 }
 
 bool add_label(struct label *l)
@@ -140,6 +306,7 @@ bool add_label(struct label *l)
 		labels = malloc(sizeof(void*)*10);
 		if (labels == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		labels_size = 10;
@@ -150,9 +317,10 @@ bool add_label(struct label *l)
 	}
 	if (next_label >= labels_size)
 	{
-		struct label* new_labels = malloc(labels_size*2);
+		struct label** new_labels = malloc(labels_size*2);
 		if (new_labels == NULL)
 		{
+			error = "Out of Memory!";
 			return false;
 		}
 		for (uint32_t i = 0;i<labels_size*2;i++)
@@ -168,6 +336,7 @@ bool add_label(struct label *l)
 		labels_size = labels_size*2;
 	}
 	labels[next_label] = l;
+	return true;
 }
 
 
@@ -182,6 +351,7 @@ char* prepare_string(const char* string)
 	char* mod_str = malloc(strlen(string)+1); // count the null byte; our string will only be cut down, so it cannot be bigger than the input
 	if (mod_str == NULL)
 	{
+		error = "Out of Memory!";
 		return NULL;
 	}
 	memset(mod_str,'\0',strlen(string)+1);
@@ -194,6 +364,7 @@ char* prepare_string(const char* string)
 		{
 			printf("prepare_string: mod_index bigger than string length!\n");
 			free(mod_str);
+			error = "prepare_string: mod_index bigger than string length!\n";
 			return NULL;
 		}
 		char c = string[i];
@@ -255,9 +426,10 @@ bool do_fixup()
 	return true;
 }
 
+
+
 void* assemble_string(const char* string)
 {
-	struct label *labels = NULL;
 	bool thumb = false;
 	char* mod_str = prepare_string(string);
 	if (mod_str == NULL)
@@ -268,12 +440,42 @@ void* assemble_string(const char* string)
 	
 	
 	
+	/// TODO default section is .text
+	/// TODO if a section with the specified name exists, just select it in the section directive
+	bool thumb = false;
+	int index = 0;
+	int ret = 0;
+	while (ret != -1)
+	{
+		int len = linelength(mod_str+index);
+		if (len > 1)
+		{
+			if (mod_str[index] == '.')
+			{
+				directive_section
+			}
+		}
+		
+	}
 	
 	
 	
 	
 	
 	
+	
+	if (sections != NULL)
+	{
+		free(sections);
+	}
+	if (fixups != NULL)
+	{
+		free(fixups);
+	}
+	if (labels != NULL)
+	{
+		free(labels);
+	}
 	free(mod_str);
 	return NULL;
 }
@@ -290,7 +492,7 @@ void* assemble_string(const char* string)
 int main(void)
 {
 	
-	assemble_string("  		mov r0, #1\n \n\n\n\n	\n");
+	assemble_string(".text\n.arm\n add r0, r0 #1");
 	
 	//asm("bkpt \n"
 	//"test: .long 0x2800000");
