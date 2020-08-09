33a4c0371bd1835121a780a4e2e5e7bf56d5fab6
nspiredev500 <57017842+nspiredev500@users.noreply.github.com>
2020-08-09 23:18:47 +0200

    re-added location for error output

--- n-as/main.c
+++ n-as/main.c
@@ -67,17 +67,15 @@
 		printf("%s: at %d:%d\n",error,line_count,char_count);
 	}
 #else
-	/*
+	
 	int main()
 	{
 		uint32_t size = 0;
 		bool thumb = false;
 		void* block = NULL;
 		uint32_t entry_offset = 0;
-		assemble_string(".data\n.zero 8\n.text\n.entry en\n.arm\n.zero 4\nen: push {r13-r14}",0,&size,&block,&entry_offset,&thumb);
+		assemble_string(".entry start\n.text\nstart: bkpt\nmov r0, dfd\nbx lr",0,&size,&block,&entry_offset,&thumb);
 		printf("%s\n",asm_error_msg);
-		printf("entry offset: %d\n",entry_offset);
-		printf("thumb: %d\n",(int) thumb);
 		FILE* f = fopen("sectiondump","wb");
 		if (f != NULL)
 		{
@@ -95,7 +93,7 @@
 		}
 		return 0;
 	}
-	*/
+	
 	int assemble_string(const char* string, uint16_t flags, uint32_t* size_ret, void** mem,uint32_t* entry_offset, bool* thumb)
 	{
 		memset(asm_error_msg,'\0',195);
@@ -116,9 +114,19 @@
 		if (current_section != -1 && parse_ret == 0)
 		{
 			int size = arrange_sections();
+			if (size == 0)
+			{
+				yyerror("empty program");
+				free_data();
+				*size_ret = 0;
+				*mem = NULL;
+				*entry_offset = 0;
+				return -1;
+			}
 			void* binary = malloc(size);
 			if (binary == NULL)
 			{
+				yyerror("Out of Memory!");
 				*size_ret = 0;
 				*mem = NULL;
 				*entry_offset = 0;
@@ -129,9 +137,17 @@
 			if (apply_fixups())
 			{
 				assemble_binary(binary,size);
-				*size_ret = size;
-				*mem = binary;
 				struct label* entry = find_label(entry_label);
+				if (strlen(entry_label) == 0)
+				{
+					yyerror("entry label not defined");
+					free_data();
+					*size_ret = 0;
+					*mem = NULL;
+					*entry_offset = 0;
+					free(binary);
+					return -1;
+				}
 				if (entry == NULL)
 				{
 					yyerror("entry label not found");
@@ -139,6 +155,7 @@
 					*size_ret = 0;
 					*mem = NULL;
 					*entry_offset = 0;
+					free(binary);
 					return -1;
 				}
 				if (entry->section == -1)
@@ -148,6 +165,7 @@
 					*size_ret = 0;
 					*mem = NULL;
 					*entry_offset = 0;
+					free(binary);
 					return -1;
 				}
 				if (entry->thumb)
@@ -158,6 +176,8 @@
 				{
 					*thumb = false;
 				}
+				*size_ret = size;
+				*mem = binary;
 				*entry_offset = sections[entry->section]->offset + entry->offset;
 				free_data();
 				return 0;
@@ -173,6 +193,10 @@
 		}
 		else
 		{
+			if (parse_ret == 0)
+			{
+				yyerror("no section defined");
+			}
 			free_data();
 			*size_ret = 0;
 			*mem = NULL;
@@ -182,6 +206,6 @@
 	}
 	void yyerror(const char* error)
 	{
-		snprintf(asm_error_msg,190,"%s",error);
+		snprintf("%s: at %d:%d\n",error,line_count,char_count);
 	}
 #endif
